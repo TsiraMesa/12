ea2c2052be186324334c43c1cbc387917b1359df
TsiraMesa <ciramesablishvili@gmail.com>
2024-07-22 18:38:20 +0400

    davaleba 6

---
+++ dd.js
@@ -0,0 +1,46 @@
+let x=87; y=65;
+
+console.log(x+y)
+
+
+let student= { firstName:"tsira",
+    lastName:"mesablishvili",
+    age:32,
+    email:"tsiramesa@gmail.com",
+     marks:[12, 16, 15, 22]
+
+}
+console.log(student.firstName)
+
+
+let  m=45, n=23
+if (m>n) {
+    console.log (n+m)}
+    else {
+    console.log(n)
+    }
+
+
+
+let students= {firstName:"tsira"}
+if (firstName="tsira")
+{console.log(student.firstName, "shine and rise")}
+
+
+
+let salary=1800, bonus=1800*10/100
+if (salary=>2000)
+    {
+    console.log(bonus=0)}
+    else {console.log(bonus)}
+  
+
+let a=23, b=23-23/2, c=a+b;
+console.log(c)
+
+
+
+let phone= ["Iphone", "samsung", "pixel", "xiaomi"];
+console.log(phone [0], phone [1],phone [2],phone [3])
+
+    
